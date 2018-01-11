0ec40f64b4efc431e97ccaf92a4194945b4a6a7e
Michael Turnwall <freelance@turnwall.net>
2018-01-10 16:47:10 -0800

    Add js export and some testing files

---
+++ test/float-label.spec.js
@@ -0,0 +1,11 @@
+import test from 'ava';
+import {stub} from 'sinon';
+import floatLabel from '../src/float-label.js';
+
+test('this will always pass', t => {
+    t.pass();
+});
+
+test('this will always fail', t => {
+    t.fail();
+});
